5fc414ec4ff878114ea6fb6eb82a5b1b3f45997d
JOSE LUIS JUAREZ MARQUEZ <jljm@JOSEs-MacBook-Air.local>
2023-05-25 17:40:44 -0600

    controladores sin servicios ni conexion a BD

---
+++ src/main/java/com/accenture/demobookify/DemoBookifyApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.demobookify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBookifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBookifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/demobookify/DemoBookifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.demobookify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBookifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
